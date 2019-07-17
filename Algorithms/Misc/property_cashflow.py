ea591a21b986ec2d5df3c289bec7243d51683163
Park <timpark0807@gmail.com>
2019-07-17 10:35:44 -0700

    handle negative cashflow on real estate app

---
+++ Algorithms/Misc/property_cashflow.py
@@ -0,0 +1,18 @@
+def cash_flow_30_year(self, income_growth, expense_growth):
+        
+    year_list = []
+    cash_flow_list = []
+    vacancy = self.vacancy_loss()
+    annual_gross_income = (self.rent - vacancy) * 12
+    annual_expense = (self.operating_expense() * 12)
+    annual_debt_service = self.mortgage_payment() * 12
+
+    for i in range(1, 31):
+
+        annual_gross_income = annual_gross_income * income_growth
+        annual_expense = annual_expense * expense_growth
+        cash_flow = round(annual_gross_income - annual_expense - annual_debt_service)
+        year_list.append(i)
+        cash_flow_list.append(cash_flow)
+
+    return year_list, cash_flow_list
