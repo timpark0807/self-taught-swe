698f183a48e8e01a21fd55f3f27f94fbc5e7584e
Tim Park <timpark0807@gmail.com>
2019-09-08 20:31:49 -0700

    pali

---
+++ Algorithms/Pali_2.py
@@ -0,0 +1,14 @@
+def pali(s):
+    low, high = 0, len(s) - 1
+
+    while low < high:
+        if s[low] == s[high]:
+            low += 1
+            high -= 1
+        else:
+            return False
+
+    return True
+
+
+print(pali('xanxynax'))
