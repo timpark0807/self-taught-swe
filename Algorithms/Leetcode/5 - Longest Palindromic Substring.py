c3fb9e0b4cccd43843060152edf3c72cfa02f536
Park <timpark0807@gmail.com>
2019-10-14 11:54:46 -0700

    Longest Palindromic Substring

---
+++ Algorithms/Leetcode/5 - Longest Palindromic Substring.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = "" 
+        for i in range(len(s)):
+            odd = self.expand(s, i, i)
+            if len(odd) > len(res):
+                res = odd
+
+            even = self.expand(s, i, i+1)
+            if len(even) > len(res):
+                res = even
+
+        return res            
+
+    def expand(self, s, left, right):
+        
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+
+            left -= 1
+            right += 1
+                
+        return s[left+1:right]
+    
+
+s = Solution()
+ans = s.longestPalindrome('addedd')
+print(ans)
