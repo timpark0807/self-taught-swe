99ac3d0d0dd87e528c43c9143b8471f6822ff2ea
Tim Park <timpark0807@gmail.com>
2019-11-10 21:14:54 -0800

    Number of Islands

--- Algorithms/Leetcode/90 - Subsets II.py
+++ Algorithms/Leetcode/90 - Subsets II.py
@@ -1,16 +1,10 @@
 class Solution(object):
+    
     def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums = sorted(nums)
-
         answers = []
         seen = set()
         self.backtrack(nums, answers, seen, [], 0)
         return answers
-    
     
     def backtrack(self, nums, answers, seen, curr, start):        
         if curr not in answers:
@@ -21,3 +15,22 @@ class Solution(object):
                 self.backtrack(nums, answers, seen, curr+[nums[i]], i+1)
                 seen.remove(i)
         
+
+    def subsets(self, nums):
+        answer = []
+        self.backtrack(nums, answer, [], 0)
+        return answer
+    
+    def backtrack(self, nums, answer, subset, i):
+        answer.append(list(subset))
+        
+        for index in range(i, len(nums)):
+            subset.append(nums[index])
+            self.backtrack(nums, answer, subset, index+1)
+            subset.pop()
+
+
+
+            
+            
+            
--- Algorithms/Median Of Two Sorted Arrays.py
+++ Algorithms/Median Of Two Sorted Arrays.py
@@ -3,6 +3,12 @@ import unittest
 class Solution:
 
     def median_of_arrays(self, arr1, arr2):
+        """
+        Clarifications:
+        -> Are arrays same length?
+        -> Are there only integer values in the array?
+        -> How do we handle an even size
+        """
         p1, p2 = 0, 0
         new_arr = []
         
@@ -16,20 +22,42 @@ class Solution:
 
         new_arr.extend(arr1[p1:])
         new_arr.extend(arr2[p2:])
-
         low, high = 0, len(new_arr) - 1
 
         while low <= high:
             if low == high:
                 return new_arr[low]
-            
             low += 1
             high -=1
 
         return (new_arr[low] + new_arr[high]) / 2
 
+    def median_same_length(self, arr1, arr2):
+        count = 0
+        p1, p2 = 0, 0
+        total_length = len(arr1) + len(arr2)
+        medians = ((total_length // 2) - 1 , total_length//2)
 
-
+        while count < medians[0]:
+            if arr1[p1] < arr2[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+            count += 1
+            
+        med = []
+        
+        while count > 0:
+            if arr1[p1] < arr2[p2]:
+                med.append(arr1[p1])
+                p1 += 1
+            else:
+                med.append(arr2[p2])
+                p2 += 1
+            count -= 1
+        
+        return (med[0] + med[1]) / 2
+        
 class TestSolution(unittest.TestCase):
     def setUp(self):
         self.s = Solution()
@@ -44,6 +72,12 @@ class TestSolution(unittest.TestCase):
         answer = self.s.median_of_arrays(arr1, arr2)
         self.assertEqual(answer, 3.5)
 
+    def test_same_length_median(self):
+        arr1 = [1, 3, 5, 7]
+        arr2 = [2, 4, 6, 8]
+
+        answer = self.s.median_same_length(arr1, arr2)
+        self.assertEqual(answer, 4.5)
 
     def test_odd_median(self):
         arr1 = [1, 3, 5]
