b14cb41a5a24905e6634a5826b57eb0f2b4d8860
Tim Park <timpark0807@gmail.com>
2019-12-02 14:03:48 -0800

    DP bottom up

--- Algorithms/Leetcode/416 - Partition Equal Subset Sum.py
+++ Algorithms/Leetcode/416 - Partition Equal Subset Sum.py
@@ -1,35 +1,25 @@
 class Solution(object):
     def canPartition(self, nums):
-        """
-        Explanation for memoization needs to be reviewed:
-        
-        1. https://leetcode.com/problems/partition-equal-subset-sum/discuss/90610/Python-Backtracking-with-Memoization-Solution
-        2. https://leetcode.com/problems/partition-equal-subset-sum/discuss/90640/simple-dfs-python-with-memo-beats-98
-        """
         if not nums:
             return True
         elif max(nums) > sum(nums) // 2:
             return False
         elif sum(nums) % 2 != 0:
             return False
-        
+            
         target = sum(nums) // 2
-        seen = set()
-        return self.dfs(nums, target, seen, 0, {})
-        
-    def dfs(self, nums, target, seen, curr_sum, d):
-        if curr_sum in d: 
-            return d[curr_sum]
-        if curr_sum == target:
-            d[curr_sum] = True
-        else:
-            d[curr_sum] = False
-            for index in range(len(nums)):
-                if index not in seen and curr_sum + nums[index] <= target:
-                    seen.add(index)
-                    if self.dfs(nums, target, seen, curr_sum + nums[index], d):
-                        d[curr_sum] = True
-                        break
-                    seen.remove(index)
 
-        return d[curr_sum]
+        dp = [False for _ in range(target+1)]
+        dp[0] = True
+        
+        for num in nums:
+            for j in reversed(range(1, len(dp))):
+                if j >= num:
+                    dp[j] = dp[j] or dp[j-num]
+                    
+
+        return dp[-1]
+
+s = Solution()
+nums = [2, 2, 3, 5]
+print(s.canPartition(nums))
