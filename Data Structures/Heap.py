3da58e5162a8948e911a32ddafa73e5b8d9f12e3
Park <timpark0807@gmail.com>
2020-02-05 08:41:41 -0800

    Remove extra heap implemenatation and update README.

--- Data Structures/Heap.py
+++ Data Structures/Heap.py
@@ -1,5 +1,3 @@
-import unittest
-
 class Heap:
     """
     Implementation of a Max Heap.
@@ -12,9 +10,11 @@ class Heap:
     |   Operation      | Time | Space |
     +------------------+------+-------+
     | Insert           | O(1) |  O(1) |
-    | Pop Max          | O(1) |  O(1) |
-    | Get Max          | O(1) |  O(1) |
     | Heap Sort        | O(n) |  O(1) |
+    | Traversal - DFS  | O(n) |  O(n) |
+    | Traversal - BFS  | O(n) |  O(n) |
+    | Topoligcal Sort  | O(n) |  O(n) |
+    | Is Cycle?        | O(n) |  O(n) |  
     +------------------+------+-------+
     """
     
@@ -22,27 +22,21 @@ class Heap:
         self.heap = heap
         self.size = len(self.heap)
    
-    def heapsort(self):
+    def build_max_heapify(self):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Builds a max heap from an unsorted array by calling max_heapify on the specified indicies.
-            - We use size // 2 because all indicies greater than half the size are leaves.
+        Builds a max heap from an unsorted array
+
+        max_heapify indexes less than half the arrays size.
+        All indexes greater than half the array are leaves. 
         """
         for index in reversed(range(0, self.size // 2)):
             self.max_heapify(index)
             
     def max_heapify(self, index):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Swaps largest value between index, left child, and right child into the index position.
-            - Recursively calls max_heapify on the index until the largest value is already in the index position.
-        """       
+        Swaps largest value between index, left child, and right child into the index position.
+        Recursively calls max_heapify on the index until the largest value is already in the index position.
+        """
         left = index * 2 + 1
         right = index * 2 + 2
         largest = index
@@ -57,49 +51,33 @@ class Heap:
 
     def _swap(self, i, j):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Internal helper function to swap the values at two indices.
+        Internal helper function to swap values at two indices.
         """
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+        return
 
     def insert(self, value):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Allows us to insert new values into an existing max heap.
-            - Heaps must be complete by definition, so we insert the value into the last position of the array.
-            - We call bubble up to place the value in the correct position. 
+        Allows us to insert new values into an existing max heap.
+        Heaps must be complete by definition, so we insert the value into the last position.
+        We call bubble up to place the value in the correct position. 
         """
         self.heap.append(value)
         self._bubble_up(self.size)
         
     def _bubble_up(self, index):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Helper function to swap the newly inserted value into the correct max position 
+        Places the inserted value in the correct max position 
         """
         while index > 0:
-            parent = (index-1)//2
-            if self.heap[index] > self.heap[parent]:
-                self._swap(index, parent)
-            index = parent
+            if self.heap[index] > self.heap[(index-1)//2]:
+                self._swap(index, (index-1)//2)
+            index = (index-1)//2
 
     def pop_max(self):
         """
-        Input  :  None
-        Output :  int
-        
-        Description:
-            - Returns the max value from the heap.
-            - Swaps the last index of the array to the root index and bubbles it down to the correct position.
+        Allows us to remove the max value from the max heap.
+        Swap the last value of the array to the root index, and bubble down into correct position.
         """
         self._swap(0, self.size)
         value = self.heap.pop()
@@ -108,25 +86,15 @@ class Heap:
 
     def _bubble_down(self, index):
         """
-        Input  :  None
-        Output :  None
-        
-        Description:
-            - Helper function that swaps the max value of index, left, and right into the index position.
+        Swaps the max value of index, left, and right into the index position.
         """
         while (index * 2) + 2 < self.size:
-            max_value = self._get_max_child(index)
+            max_value = self._get_max(index)
             self._swap(index, max_value)
             index = max_value
+            print(max_value)
     
-    def _get_max_child(self, index):
-        """
-        Input  :  None
-        Output :  int
-        
-        Description:
-            - Helper function that returns the max index between the current index, the left, and the right child.
-        """
+    def _get_max(self, index):
         if index * 2 + 1 > self.size:   # if index has no chidren, return index
             return index
         else:
@@ -136,21 +104,11 @@ class Heap:
                 return index * 2 + 2
 
 
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        arr = [i for i in range(1,10,2)]
-        self.h = Heap(arr)
-        self.h.heapsort()
-
-    def tearDown(self):
-        self.h = None
-
-    def test_build_max_heapify(self):
-        self.assertEqual(self.h.heap, [19, 13, 15, 17, 11, 5, 1, 7, 3, 9])
-        
-    def test_insert(self):
-        self.h.insert(55)
-        self.assertEqual(self.h.heap, [55, 7, 9, 1, 3, 5])
-
 if __name__ == '__main__':
-    unittest.main()
+    arr = [i for i in range(1,20,2)]
+    heap = Heap(arr)
+    heap.build_max_heapify()
+    heap.insert(55)
+    print(heap.heap)
+    print(heap.pop_max())
+    print(heap.heap)
--- Data Structures/README.md
+++ Data Structures/README.md
@@ -2,17 +2,10 @@
 
 This repository showcases my implementation of the following Data Structures in Python.
 
-Completed:
-- Linked List
-- Graph 
 - Binary Tree
-
-TODO:
-- Stack
-- Queue
+- Graph 
 - Heap
-
-- Binary Search Tree
-
+- Linked List
+- Stack
 
 Unittests for each data structure are contained in the tests folder. 
